245812826250a7b2b097a25e604f782e284484ed
lishaoting007 <lishaoting_007@163.com>
2019-03-27 22:14:42 +0800

    写到axios

---
+++ src/components/my-swiper/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <swiper :options="swiperOption" ref="mySwiper">
+      <!-- slides -->
+      <swiper-slide>I'm Slide 1</swiper-slide>
+      <swiper-slide>I'm Slide 2</swiper-slide>
+      <swiper-slide>I'm Slide 3</swiper-slide>
+      <swiper-slide>I'm Slide 4</swiper-slide>
+      <swiper-slide>I'm Slide 5</swiper-slide>
+      <swiper-slide>I'm Slide 6</swiper-slide>
+      <swiper-slide>I'm Slide 7</swiper-slide>
+      <!-- Optional controls -->
+      <div class="swiper-pagination" slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+import "swiper/dist/css/swiper.css";
+import { swiper, swiperSlide } from "vue-awesome-swiper";
+export default {
+  name: "index",
+  components: {
+    swiper,
+    swiperSlide
+  },
+  data() {
+    return {
+      swiperOption: {}
+    };
+  }
+};
+</script>
+
+<style scoped>
+</style>
---
+++ src/views/index/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    首页
+    <my-swiper></my-swiper>
+  </div>
+</template>
+
+<script>
+import MySwiper from "@/components/my-swiper/index";
+export default {
+  name: "index",
+  components: {
+    "my-swiper": MySwiper
+  },
+  data() {
+    return {
+      swiperOption: {}
+    };
+  }
+};
+</script>
+
+<style scoped lang='scss'>
+</style>
